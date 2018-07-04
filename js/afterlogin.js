4b251c035baa42e2c721bbb1739574fef7072ea1
vinod <vinodlkaradiya@gmail.com>
2018-07-04 15:18:01 +0530

    login page , sinup page designed

---
+++ js/afterlogin.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+    $('#myfooter').load('/templates/footer.html');
+    $('#navigationbar').load('/templates/navigationbar2.html');
+});
---
+++ js/mysidebar.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+    $('#mysidebar').load('../templates/mysidebar.html');
+});
