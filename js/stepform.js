d710b3a64721513442dc95c75807daa928744efa
vinod <vinodlkaradiya@gmail.com>
2018-07-11 14:09:51 +0530

    step form address field added

--- js/stepform.js
+++ js/stepform.js
@@ -12,27 +12,33 @@ var $FORM1 = $('#form1');
 var $FORM2 = $('#form2');
 var $FORM3 = $('#form3');
 var $FORM4 = $('#form4');
+var $FORM5 = $('#form5');
+
 var $FULLDETAILS = $('#fulldetials');
 
 var $STEP1 = $('#step1');
 var $STEP2 = $('#step2');
 var $STEP3 = $('#step3');
 var $STEP4 = $('#step4');
+var $STEP5 = $('#step5');
 
 var $BACK1 = $('#back1');
 var $BACK2 = $('#back2');
 var $BACK3 = $('#back3');
 var $BACK4 = $('#back4');
+var $BACK5 = $('#back5');
 
 $STEP1.on('click', _funcN1);
 $STEP2.on('click', _funcN2);
 $STEP3.on('click', _funcN3);
 $STEP4.on('click', _funcN4);
+$STEP5.on('click', _funcN5);
 
 $BACK1.on('click',_funcB1);
 $BACK2.on('click',_funcB2);
 $BACK3.on('click',_funcB3);
 $BACK4.on('click',_funcB4);
+$BACK5.on('click',_funcB5);
 
 $ADDMOREJOBS.on('click',_funcAddJob);
 $ADDMORESKILLS.on('click',_funcAddSkills);
@@ -50,37 +56,43 @@ function nextStep(showform, hideform) {
 }
 
 function _funcN1() {
-    nextStep($FORM2, $FORM1)
+    nextStep($FORM2, $FORM1);
 }
 
 function _funcN2() {
-    nextStep($FORM3, $FORM2)
+    nextStep($FORM3, $FORM2);
 }
 
 function _funcN3() {
-    nextStep($FORM4, $FORM3)
+    nextStep($FORM4, $FORM3);
 }
 
 function _funcN4() {
-    var data = Get_All_Page_Data();
-    showAllDetials(data);
-    nextStep($FULLDETAILS, $FORM4)
+    nextStep($FORM5, $FORM4);
+}
+
+function _funcN5() {
+    nextStep($FULLDETAILS, $FORM5);
 }
 
 
 function _funcB1() {
-    nextStep($FORM1, $FORM2)
+    nextStep($FORM1, $FORM2);
 }
 function _funcB2() {
-    nextStep($FORM2, $FORM3)
+    nextStep($FORM2, $FORM3);
 
 }
 function _funcB3() {
-    nextStep($FORM3, $FORM4)
+    nextStep($FORM3, $FORM4);
 
 }
 function _funcB4() {
-    nextStep($FORM4, $FULLDETAILS)
+    nextStep($FORM4, $FORM5);
+
+}
+function _funcB5() {
+    nextStep($FORM5, $FULLDETAILS);
 
 }
 
