b72ce8ee661a9c0077bf235bb1301d053d792578
vinod <vinodlkaradiya@gmail.com>
2018-07-24 16:21:39 +0530

    profile page design updated

--- static/js/profiletab.js
+++ static/js/profiletab.js
@@ -12,19 +12,26 @@ tab3.on("click", showpanel3);
 tab4.on("click", showpanel4);
 
 function showpanel1() {
-    showProfileTab($("#panel1"))
+    showProfileTab($("#panel1"));
+    $(this).addClass('active-tab').siblings().removeClass('active-tab');
 };
 
 function showpanel2() {
-    showProfileTab($("#panel2"))
+    showProfileTab($("#panel2"));
+    $(this).addClass('active-tab').siblings().removeClass('active-tab');
+
 };
 
 function showpanel3() {
-    showProfileTab($("#panel3"))
+    showProfileTab($("#panel3"));
+    $(this).addClass('active-tab').siblings().removeClass('active-tab');
+
 };
 
 function showpanel4() {
-    showProfileTab($("#panel4"))
+    showProfileTab($("#panel4"));
+    $(this).addClass('active-tab').siblings().removeClass('active-tab');
+
 };
 
 function showProfileTab(showpanel) {
