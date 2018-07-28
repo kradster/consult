84078c83b6e8d72a9006ad518771f5ce0a7e94ef
Devesh Aggrawal <deveshaggrawal19@gmail.com>
2018-07-28 21:20:23 +0530

    adding states in profile

--- models/profile.js
+++ models/profile.js
@@ -77,7 +77,7 @@ var Profile = new Schema({
         },
         state: {
             type: String,
-            enum: ['Andhra Pradesh', 'Uttar Pradesh']
+            enum: ['Andaman and Nicobar Islands', 'Andhra Pradesh', 'Arunachal Pradesh', 'Assam', 'Bihar', 'Chandigarh', 'Chhattisgarh', 'Dadra and Nagar Haveli', 'Daman and Diu', 'Delhi', 'Goa', 'Gujarat', 'Haryana', 'Himachal Pradesh', 'Jammu and Kashmir', 'Jharkhand', 'Karnataka', 'Kerala', 'Lakshadweep', 'Madhya Pradesh', 'Maharashtra', 'Manipur', 'Meghalaya', 'Mizoram', 'Nagaland', 'Orissa', 'Pondicherry', 'Punjab', 'Rajasthan', 'Sikkim', 'Tamil Nadu', 'Telangana', 'Tripura', 'Uttaranchal', 'Uttar Pradesh', 'West Bengal']
         },
         pincode: {
             type: Number
